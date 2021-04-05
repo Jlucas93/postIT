6b69bedfe9e690d6a8b8084f7f4180f6bbcde8b6
João Lucas <jlnascimentoandrade@gmail.com>
2021-04-05 15:00:07 -0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# AV-Post-it
+# Portfolio
+My Portfolio
+
+**tecnologia**
+CSS / HTML / JavaSricpt
+
+**autor**
+Foto | Nome | GitHub | Likedin | E-mail
+---- | ---- | ------ | ------- | ------
+<img src="./img/perfil1.jpg" width="100px">  | João Lucas Nascimento Andrade | [João Lucas Andrade](https://github.com/Jlucas93/Form) | [Linkedin](https://www.linkedin.com/in/joão-lucas-nascimento-andrade-34574398) | jlnascimentoandrade@gmail.com
---
+++ js/script.js
@@ -0,0 +1,59 @@
+//Pegando a div app
+var appElement = document.getElementById('app');
+
+//estilizando o app
+appElement.style.alignItems = "center";
+appElement.style.display = "flex";
+appElement.style.backgroundColor= "#2b2b2b";
+appElement.style.height = "100vh";
+appElement.style.flexDirection = 'column';
+appElement.style.justifyContent = "top";
+appElement.style.width = "100vw";
+
+//Criando os elementos
+var btnElement = document.createElement('button');
+var inputElement = document.createElement('input');
+
+//Definindo atributos para o botão
+btnElement.setAttribute('id', 'nome');
+var txt = document.createTextNode('Criar post it');
+btnElement.appendChild(txt);
+
+//Definindo atributos para input
+inputElement.setAttribute('id', 'nome');
+inputElement.setAttribute('type', 'nome');
+inputElement.setAttribute('placeholder', 'Digite o lembrete');
+
+//Criando a função para gerar um quadrado e receber o valor do input
+function postit(){
+    var squareElement = document.createElement('div');
+    squareElement.setAttribute('id','square')
+    
+    var texts = document.createTextNode(inputElement.value*5);
+    squareElement.appendChild(texts);
+    
+    //Estilizando o post it
+    squareElement.style.width = '200px';
+    squareElement.style.height = '200px';
+    squareElement.style.backgroundColor = 'yellow';
+    squareElement.style.margin = '5px';
+    squareElement.style.fontSize = '35px';
+    squareElement.style.justifyContent = 'center';
+    
+    
+    appElement.appendChild(squareElement);
+}
+
+//Craidno função para limpar campo do input
+function apagar(){
+    inputElement.value = '';
+}
+
+//Linkando a função com o botão
+btnElement.onclick = function(){
+    postit();
+    apagar();
+}
+// Atribuindo a Div app
+appElement.appendChild(btnElement);
+appElement.appendChild(inputElement);
